35267ce65082b64b4d3082208aa3779489dc9c1f
Hannes Albert <hannes10042004@icloud.com>
2023-04-24 14:19:27 +0200

    Added lsp support

--- lua/hannes/packer.lua
+++ lua/hannes/packer.lua
@@ -4,30 +4,52 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-use 'wbthomason/packer.nvim'
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.1',
--- or                            , branch = '0.1.x',
-  requires = { {'nvim-lua/plenary.nvim'} }
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	-- install colorscheme
+	use 'folke/tokyonight.nvim'
+
+	use { "ellisonleao/gruvbox.nvim" }
+
+	-- install treesitter
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+	-- install treesitter playground
+	use('nvim-treesitter/playground')
+
+	-- install harpoom
+	use('theprimeagen/harpoon')
+
+	-- install fugitive
+	use('tpope/vim-fugitive')
+
+	-- insatll LSP
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{                                      -- Optional
+			'williamboman/mason.nvim',
+			run = function()
+				pcall(vim.cmd, 'MasonUpdate')
+			end,
+		},
+		{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+		-- Autocompletion
+		{'hrsh7th/nvim-cmp'},     -- Required
+		{'hrsh7th/cmp-nvim-lsp'}, -- Required
+		{'L3MON4D3/LuaSnip'},     -- Required
+	}
 }
-
--- install colorscheme
-use 'folke/tokyonight.nvim'
-
--- install treesitter
-use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-
--- install treesitter playground
-use('nvim-treesitter/playground')
-
--- install harpoom
-use('theprimeagen/harpoon')
-
--- install fugitive
-use('tpope/vim-fugitive')
-
-use { "ellisonleao/gruvbox.nvim" }
 
 end)
