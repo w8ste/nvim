c9de90d7f9879efbc054baf9695767b8f9cb4b9b
Hannes Albert <hannes10042004@icloud.com>
2023-10-06 00:57:07 +0200

    Update ibl

--- after/plugin/indent_blankline.lua
+++ after/plugin/indent_blankline.lua
@@ -1,6 +1,6 @@
-require("indent_blankline").setup {
+require("ibl").setup {
     -- for example, context is off by default, use this to turn it on
-    show_current_context = true,
-    show_current_context_start = true,
-    filetype_exclude = {'dashboard'}
+    --show_current_context = true,
+    --show_current_context_start = true,
+    --filetype_exclude = {'dashboard'}
 }
